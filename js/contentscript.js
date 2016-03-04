346464c839de38f7f59764352fbcbafc795fbf10
mnsekh111 <mnsekh111@gmail.com>
2016-03-04 13:52:36 -0500

    Remove duplicate tabs completed

---
+++ js/contentscript.js
@@ -0,0 +1,4 @@
+/**
+ * Created by mnsekh111 on 2/17/2016.
+ */
+console.log("Content script loaded");
--- src/bg/background.js
+++ src/bg/background.js
@@ -5,16 +5,23 @@
 // });
 
 
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
-
-
 /*Set a browser action on click listener*/
-chrome.browserAction.onClicked(function(tab){
+chrome.browserAction.onClicked.addListener(function (tab) {
+    var unique = [];
+
+    /*Asynchronous call to retrieve the tabs opened in this window */
+
+    chrome.tabs.query({currentWindow: true}, function (tabs) {
+        var i,element;
+
+        for(i=0;i<tabs.length;i++){
+            element = tabs[i];
+            if(unique.indexOf(element.url) == -1)
+                unique.push(element.url);
+            else
+                chrome.tabs.remove(element.id,function(){});
+        }
+    });
+
 
 });
