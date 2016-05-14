b4edd6303001528b1a8e5b456964da5719f019ac
mnsekh111 <mnsekh111@gmail.com>
2016-05-14 16:19:20 -0400

    changed indentation, Removed unnecessary content script styles

--- src/bg/background.js
+++ src/bg/background.js
@@ -7,79 +7,80 @@
 
 /*Set a browser action on click listener*/
 chrome.browserAction.onClicked.addListener(function (tab) {
-    closeSameDomain();
+        closeSameDomain();
 });
 
 
 /*Sending a notification in the content script*/
 function sendNotification(message) {
 
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {type: "notification", msg: message}, function (response) {
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {type: "notification", msg: message}, function (response) {
+                });
+                console.log("Sending the notification to content script");
         });
-        console.log("Sending the notification to content script");
-    });
 }
 
+
 function closeDuplicate() {
-    var unique = [];
+        var unique = [];
 
-    /*Asynchronous call to retrieve the tabs opened in this window */
+        /*Asynchronous call to retrieve the tabs opened in this window */
 
-    chrome.tabs.query({currentWindow: true}, function (tabs) {
-        var i, element;
+        chrome.tabs.query({currentWindow: true}, function (tabs) {
+                var i, element;
 
-        for (i = 0; i < tabs.length; i++) {
-            element = tabs[i];
-            if (unique.indexOf(element.url) == -1)
-                unique.push(element.url);
-            else
-                chrome.tabs.remove(element.id, function () {
-                });
-        }
-        sendNotification((tabs.length - unique.length) + " tabs closed ");
-    });
+                for (i = 0; i < tabs.length; i++) {
+                        element = tabs[i];
+                        if (unique.indexOf(element.url) == -1)
+                                unique.push(element.url);
+                        else
+                                chrome.tabs.remove(element.id, function () {
+                                });
+                }
+                sendNotification((tabs.length - unique.length) + " tabs closed ");
+        });
 
 }
 
 function closeSameDomain() {
-    var unique = [];
+        var unique = [];
 
-    /*Asynchronous call to retrieve the tabs opened in this window */
+        /*Asynchronous call to retrieve the tabs opened in this window */
 
-    chrome.tabs.query({currentWindow: true}, function (tabs) {
-        var i, element, domain;
+        chrome.tabs.query({currentWindow: true}, function (tabs) {
+                var i, element, domain;
 
-        for (i = 0; i < tabs.length; i++) {
-            element = tabs[i];
+                for (i = 0; i < tabs.length; i++) {
+                        element = tabs[i];
 
-            domain = extractDomain(element.url);
-            if (unique.indexOf(domain) == -1)
-                unique.push(domain);
-            else
-                chrome.tabs.remove(element.id, function () {
-                });
-        }
-        sendNotification((tabs.length - unique.length) + " tabs closed ");
-    });
+                        domain = extractDomain(element.url);
+                        if (unique.indexOf(domain) == -1)
+                                unique.push(domain);
+                        else
+                                chrome.tabs.remove(element.id, function () {
+                                });
+                }
+                sendNotification((tabs.length - unique.length) + " tabs closed ");
+        });
 
 }
 
 
 function extractDomain(url) {
-    var domain;
-    //find & remove protocol (http, ftp, etc.) and get domain
-    if (url.indexOf("://") > -1) {
-        domain = url.split('/')[2];
-    }
-    else {
-        domain = url.split('/')[0];
-    }
+        var domain;
+        //find & remove protocol (http, ftp, etc.) and get domain
+        if (url.indexOf("://") > -1) {
+                domain = url.split('/')[2];
+        }
+        else {
+                domain = url.split('/')[0];
+        }
 
-    //find & remove port number
-    domain = domain.split(':')[0];
+        //find & remove port number
+        domain = domain.split(':')[0];
 
-    return domain;
+        return domain;
 }
 
 
