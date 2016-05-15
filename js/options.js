8f5dacd7e2c6d47e6c082ed87a4aa13a9d15889d
mnsekh111 <mnsekh111@gmail.com>
2016-05-14 23:03:38 -0400

    Async comm from event script to background.js

--- js/options.js
+++ js/options.js
@@ -1,27 +1,44 @@
 var glob_options = {
         TAB_KEEP: "oldest",
-        SCHEME:"sameurl"
+        SCHEME: "sameurl"
 };
 
 $(document).ready(function () {
-        restore_options(function(options){
+        restore_options(function (options) {
                 glob_options = options;
                 if (glob_options["TAB_KEEP"] == "recent")
-                                document.getElementById("recent").checked = true;
+                        document.getElementById("recent").checked = true;
+                else
+                        document.getElementById("oldest").checked = true;
+
                 if (glob_options["SCHEME"] == "samedomain")
                         document.getElementById("samedomain").checked = true;
+                else
+                        document.getElementById("sameurl").checked = true;
+                sendToBackGround();
         });
         $("#btnSave").click(function () {
 
                 if (document.getElementById("recent").checked) {
                         glob_options["TAB_KEEP"] = "recent";
+                } else {
+                        glob_options["TAB_KEEP"] = "oldest";
                 }
 
-                if(document.getElementById("samedomain").checked)
+                if (document.getElementById("samedomain").checked)
                         glob_options["SCHEME"] = "samedomain";
-
-                save_options(glob_options);
+                else {
+                        glob_options["SCHEME"] = "sameurl";
+                }
+                save_options(glob_options, function () {
+                        sendToBackGround();
+                });
         });
 });
+
+function sendToBackGround() {
+        chrome.runtime.sendMessage({GOPTIONS: glob_options}, function (response) {
+        });
+}
 
 
