6ba12514d8c422c5bb028ad083443403ee8da848
mnsekh111 <mnsekh111@gmail.com>
2016-05-14 20:16:01 -0400

    Added logic to keep the most recently opened duplicate tab

--- src/bg/background.js
+++ src/bg/background.js
@@ -7,7 +7,11 @@
 
 /*Set a browser action on click listener*/
 chrome.browserAction.onClicked.addListener(function (tab) {
-        closeSameDomain();
+        if (glob_options["SCHEME"] == "samedomain")
+                closeSameDomain();
+        else
+                closeDuplicate();
+
 });
 
 
@@ -23,49 +27,82 @@ function sendNotification(message) {
 
 
 function closeDuplicate() {
-        var unique = [];
-
         /*Asynchronous call to retrieve the tabs opened in this window */
 
-        chrome.tabs.query({currentWindow: true}, function (tabs) {
-                var i, element;
+        if (glob_options["TAB_KEEP"] == "oldest") {
+                var unique = [];
+                chrome.tabs.query({currentWindow: true}, function (tabs) {
+                        var i, element;
 
-                for (i = 0; i < tabs.length; i++) {
-                        element = tabs[i];
-                        if (unique.indexOf(element.url) == -1)
-                                unique.push(element.url);
-                        else
-                                chrome.tabs.remove(element.id, function () {
-                                });
-                }
+                        for (i = 0; i < tabs.length; i++) {
+                                element = tabs[i];
+                                if (unique.indexOf(element.url) == -1)
+                                        unique.push(element.url);
+                                else
+                                        chrome.tabs.remove(element.id, function () {
+                                        });
+                        }
 
-                setTimeout(function(){sendNotification((tabs.length - unique.length) + " tabs closed ")}, 1000);
+                        setTimeout(function () {
+                                sendNotification((tabs.length - unique.length) + " tabs closed ")
+                        }, 1000);
 
-        });
+                });
+        } else if (glob_options["TAB_KEEP"] == "recent") {
+                var unique = {}
+                chrome.tabs.query({currentWindow: true}, function (tabs) {
+                        var i, element;
+                        for (i = 0; i < tabs.length; i++) {
+                                element = tabs[i];
+                                if (unique[element.url] == undefined)
+                                        unique[element.url] = element;
+                                else {
+                                        var temp = unique[element.url].id
+                                        if (element.id < temp) {
+                                                unique[element.url] = element;
+                                                chrome.tabs.remove(temp.id, function () {
+                                                });
+                                        } else {
+                                                chrome.tabs.remove(element.id, function () {
+                                                });
+                                        }
+                                }
+                        }
+
+                        setTimeout(function () {
+                                sendNotification((tabs.length - unique.length) + " tabs closed ")
+                        }, 1000);
+
+                });
+        }
+
 
 }
 
+
 function closeSameDomain() {
-        var unique = [];
 
         /*Asynchronous call to retrieve the tabs opened in this window */
+        if (glob_options["TAB_KEEP"] == "oldest") {
+                var unique = [];
+                chrome.tabs.query({currentWindow: true}, function (tabs) {
+                        var i, element, domain;
+                        for (i = 0; i < tabs.length; i++) {
+                                element = tabs[i];
+                                console.log(tabs[i].id + " " + tabs[i].title)
+                                domain = extractDomain(element.url);
+                                if (unique.indexOf(domain) == -1)
+                                        unique.push(domain);
+                                else
+                                        chrome.tabs.remove(element.id, function () {
+                                        });
+                        }
 
-        chrome.tabs.query({currentWindow: true}, function (tabs) {
-                var i, element, domain;
-
-                for (i = 0; i < tabs.length; i++) {
-                        element = tabs[i];
-                        console.log(tabs[i].id + " " + tabs[i].title)
-                        domain = extractDomain(element.url);
-                        if (unique.indexOf(domain) == -1)
-                                unique.push(domain);
-                        else
-                                chrome.tabs.remove(element.id, function () {
-                                });
-                }
-
-                setTimeout(function(){sendNotification((tabs.length - unique.length) + " tabs closed ")}, 1000);
-        });
+                        setTimeout(function () {
+                                sendNotification((tabs.length - unique.length) + " tabs closed ")
+                        }, 1000);
+                });
+        }
 
 }
 
